718fcaae91ed03e3665439a5e19a626a2d398449
Alex Vongseng <alex.vongseng@gmail.com>
2023-08-24 12:36:01 -0500

    Initialized prisma schema

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,7 @@ export default function Home() {
     <main className='flex flex-col min-h-screen'>
       <Nav />
       <div className='flex flex-col justify-center max-w-5xl mx-auto flex-grow w-full px-4'>
-        <Card className='flex items-center bg-primary-foreground gap-4 md:gap-8 p-4 md:p-8 border-[3px] rounded-3xl md:rounded-[30px] '>
-          <Checkbox 
-            checked={checked}
-            onCheckedChange={() => setChecked(!checked)}
-            className='w-9 md:w-12 h-9 md:h-12 rounded-[13px] md:rounded-[20px] border-[3px]' 
-          />
-          <div>
-            <h3 className={`md:text-xl font-medium ${checked ? 'line-through' : ''}`}>do it</h3>
-            <p className='text-sm md:text-lg text-muted-foreground'>Coming Soon</p>
-          </div>
-        </Card>
+        hello
       </div>
     </main>
   )
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,18 +1,20 @@
 import { GitHubLogoIcon } from "@radix-ui/react-icons"
+import { ThemeToggle } from "./theme-toggle"
 import Link from 'next/link'
 
 const Nav = () => {
   return (
-    <nav className='flex justify-between border-b py-4'>
+    <nav className='flex justify-between border-b py-4 sticky top-0 bg-primary-foreground'>
         <div className="flex justify-between items-center max-w-5xl mx-auto px-4 flex-grow">
             <div className='flex items-center gap-2'>
                 <div className='w-8 h-8 border-[3px] rounded-[13px]' />
                 <h1 className='text-lg font-medium text-muted-foreground'>do it</h1>
             </div>
-            <div>
+            <div className="flex gap-2 items-center">
                 <Link href="http://github.com/alex-von/do-it.git" target="_blank">
                     <GitHubLogoIcon className='w-6 h-6 text-muted-foreground' />
                 </Link>
+                <ThemeToggle />
             </div>
         </div>
     </nav>
